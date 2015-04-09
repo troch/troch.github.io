016dc5b7ac299f17401f3eeaa69fb691df960c0a
Thomas Roch <thomas.c.roch@gmail.com>
2015-04-09 12:30:38 +0100

    Develop skeleton and test private folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
+var markdown = require('gulp-markdown');
 var nunjucks = require('nunjucks');
 var map = require('map-stream');
+var argv = require('yargs').argv;
+var slug = require('slug');
+var fs = require('fs');
+var _ = require('lodash');
 
 gulp.task('build', ['less', 'index']);
 
@@ -24,4 +29,58 @@ gulp.task('index', function () {
             });
         }))
         .pipe(gulp.dest(''));
+});
+
+gulp.task('articles', function () {
+    var articles = require('./articles.json');
+
+    return gulp.src('articles/published/*.md')
+        .pipe(markdown({gfm: true}))
+        .pipe(map(function (file, cb) {
+            // _.
+            console.log(file.cwd);
+            cb(null, file);
+        }));
+});
+
+gulp.task('createArticle', function (done) {
+    if (!argv.name) {
+        done("Missing article name");
+        return;
+    }
+
+    var articles = require('./articles.json');
+    var slugName = slug(argv.name).toLowerCase();
+
+    if(_.find(articles, {name: slugName})) {
+        done("Article name already exists");
+        return;
+    }
+
+    articles.push({
+        name: slugName,
+        published: false
+    });
+
+    fs.writeFile('articles/unpublished/' + slugName + '.md', '#' + argv.name, function (err) {
+        if (err) done(err);
+        fs.writeFile('articles.json', JSON.stringify(articles), done);
+    });
+});
+
+gulp.task('deleteArticle', function (done) {
+    if (!argv.name) {
+        done("Missing article name");
+        return;
+    }
+
+    var articles = require('./articles.json');
+    var slugName = slug(argv.name).toLowerCase();
+
+    if(!_.find(articles, {name: slugName})) {
+        done("Article name not found");
+        return;
+    }
+
+    // Delete article
 });
