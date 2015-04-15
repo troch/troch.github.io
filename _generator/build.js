7e77c778cf6d563a2c26fbf43bfa95ef28cf8fbc
Thomas Roch <thomas.c.roch@gmail.com>
2015-04-15 23:00:47 +0100

    Template with nunjucks

--- _generator/build.js
+++ _generator/build.js
@@ -3,18 +3,27 @@ var drafts          = require('metalsmith-drafts');
 var markdown        = require('metalsmith-markdown');
 var permalinks      = require('metalsmith-permalinks');
 var templates       = require('metalsmith-templates');
+var nunjucks        = require('nunjucks');
+var _               = require('lodash');
+var fs              = require('fs');
 
 var noop = function(err) {
     if (err) throw err;
 };
 
+nunjucks.configure(__dirname + '/templates', {watch: false});
+
 Metalsmith(__dirname)
   .use(drafts())
   .use(markdown())
   .use(permalinks('posts/:title'))
-  .use(templates({
-    engine: 'nunjucks',
-    directory: 'templates'
-  }))
+  .use(function (files, metalsmith, done) {
+    for (file in files) {
+        files[file].contents = new Buffer(
+            nunjucks.render(__dirname + '/templates/index.html', {contents: files[file].contents.toString()})
+        );
+    };
+    done();
+  })
   .destination('../build')
   .build(noop);
---
+++ _generator/src/test.md
@@ -0,0 +1,9 @@
+---
+title: A blog post
+draft: false
+date: 2015-04-15
+---
+
+# Title 1
+
+## Title 2
