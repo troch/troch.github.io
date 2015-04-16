1d214ee7208cec72df241e4b86a9e1624d356f2f
Thomas Roch <thomas.c.roch@gmail.com>
2015-04-16 23:47:12 +0100

    Design layout and graphical identity

--- _generator/build.js
+++ _generator/build.js
@@ -1,29 +1,74 @@
 var Metalsmith      = require('metalsmith');
+//  Metalsmith plugins
 var drafts          = require('metalsmith-drafts');
 var markdown        = require('metalsmith-markdown');
 var permalinks      = require('metalsmith-permalinks');
 var templates       = require('metalsmith-templates');
+var metallic        = require('metalsmith-metallic');
+// Other modules
 var nunjucks        = require('nunjucks');
 var _               = require('lodash');
 var fs              = require('fs');
 
-var noop = function(err) {
-    if (err) throw err;
-};
-
+// Configure nunjucks
 nunjucks.configure(__dirname + '/templates', {watch: false});
+var styleSheets = [
+    'assets/app.css',
+    'http://fonts.googleapis.com/css?family=PT+Sans:400,700',
+    'http://yandex.st/highlightjs/8.0/styles/default.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/styles/github.min.css'
+    // 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css.map'
+];
+var scriptsSrc = [
+    'https://cdnjs.cloudflare.com/ajax/libs/lunr.js/0.5.7/lunr.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/highlight.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/languages/javascript.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/languages/css.min.js'
+];
+var scripts = [
+    'hljs.initHighlightingOnLoad();'
+];
 
+// Build
 Metalsmith(__dirname)
-  .use(drafts())
-  .use(markdown())
-  .use(permalinks('posts/:title'))
-  .use(function (files, metalsmith, done) {
-    for (file in files) {
-        files[file].contents = new Buffer(
-            nunjucks.render(__dirname + '/templates/index.html', {contents: files[file].contents.toString()})
-        );
+    .use(drafts())
+    .use(markdown({
+        gfm: true
+    }))
+    .use(replaceCodeLanguage())
+    .use(permalinks('posts/:title'))
+    .use(template())
+    .destination('../build')
+    .build(noop);
+
+// Functions
+function noop(err) {
+    if (err) throw err;
+}
+
+function replaceCodeLanguage() {
+    return function (files, metalsmith, done) {
+        for (file in files) {
+            files[file].contents = new Buffer(
+                files[file].contents.toString().replace('class="lang-', 'class="')
+            );
+        };
+        done();
     };
-    done();
-  })
-  .destination('../build')
-  .build(noop);
+}
+
+function template() {
+    return function (files, metalsmith, done) {
+        for (file in files) {
+            files[file].contents = new Buffer(
+                nunjucks.render(__dirname + '/templates/index.html', {
+                    styleSheets: styleSheets,
+                    scriptsSrc: scriptsSrc,
+                    scripts: scripts,
+                    contents: files[file].contents.toString()
+                })
+            );
+        };
+        done();
+    };
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,11 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var rename = require('gulp-rename');
-var markdown = require('gulp-markdown');
-var nunjucks = require('nunjucks');
-var map = require('map-stream');
-var argv = require('yargs').argv;
-var slug = require('slug');
-var fs = require('fs');
-var _ = require('lodash');
 
-gulp.task('build', ['less', 'index']);
+gulp.task('build', ['less']);
 
 gulp.task('less', function () {
     return gulp.src('_less/app.less')
         .pipe(less({paths: ['less']}))
         // .pipe(rename('dist.css'))
         .pipe(gulp.dest('assets'));
-});
-
-gulp.task('index', function () {
-    nunjucks.configure('_templates');
-
-    return gulp.src('_templates/index.html')
-        .pipe(map(function(file, cb) {
-            nunjucks.renderString(file.contents.toString(), function (err, res) {
-                file.contents = new Buffer(res);
-                cb(null, file);
-            });
-        }))
-        .pipe(gulp.dest(''));
-});
-
-gulp.task('articles', function () {
-    var articles = require('./_articles.json');
-
-    return gulp.src('_articles/published/*.md')
-        .pipe(markdown({gfm: true}))
-        .pipe(map(function (file, cb) {
-            // _.
-            console.log(file.cwd);
-            cb(null, file);
-        }));
-});
-
-gulp.task('createArticle', function (done) {
-    if (!argv.name) {
-        done("Missing article name");
-        return;
-    }
-
-    var articles = require('./_articles.json');
-    var slugName = slug(argv.name).toLowerCase();
-
-    if(_.find(articles, {name: slugName})) {
-        done("Article name already exists");
-        return;
-    }
-
-    articles.push({
-        name: slugName,
-        published: false
-    });
-
-    fs.writeFile('_articles/unpublished/' + slugName + '.md', '#' + argv.name, function (err) {
-        if (err) done(err);
-        fs.writeFile('_articles.json', JSON.stringify(articles), done);
-    });
-});
-
-gulp.task('deleteArticle', function (done) {
-    if (!argv.name) {
-        done("Missing article name");
-        return;
-    }
-
-    var articles = require('./_articles.json');
-    var slugName = slug(argv.name).toLowerCase();
-
-    if(!_.find(articles, {name: slugName})) {
-        done("Article name not found");
-        return;
-    }
-
-    // Delete article
 });
