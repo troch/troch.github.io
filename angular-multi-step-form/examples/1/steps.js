52383d75bb4449a60ed3cae68389fb906130035e
Thomas Roch <thomas.c.roch@gmail.com>
2015-06-13 18:10:52 +0100

    Structure examples

---
+++ angular-multi-step-form/examples/1/steps.js
@@ -0,0 +1,18 @@
+$scope.steps = [
+    {
+        templateUrl: '/angular-multi-step-form/examples/1/step1.html',
+        title: 'Get the source'
+    },
+    {
+        templateUrl: '/angular-multi-step-form/examples/1/step2.html',
+        title: 'Add it to your app'
+    },
+    {
+        templateUrl: '/angular-multi-step-form/examples/1/step3.html',
+        title: 'Create your multi step forms / wizzards'
+    },
+    {
+        template: '<div class="well">More docs available on Github</div>',
+        title: 'Read the docs'
+    }
+];
--- angular-multi-step-form/scripts/examples.js
+++ angular-multi-step-form/scripts/examples.js
@@ -2,7 +2,7 @@
 
 angular
 
-.module('msfDemo', ['ngAnimate', 'ngRoute', 'multiStepForm'])
+.module('msfDemo', ['ngAnimate', 'ngRoute', 'multiStepForm', 'ui.bootstrap', 'hljs'])
 
 .config([
     '$routeProvider',
@@ -12,33 +12,35 @@ angular
                 template: '<div class="alert alert-warning">This page is work in progress, come back to check later!</div>'
                 // controller: ''
             })
-            .when('/how-to-use', {
-                controller: 'HowToInstallCtrl',
-                templateUrl: '/angular-multi-step-form/partials/example1.html'
+            .when('/getting-started', {
+                controller: 'GettingStartedCtrl',
+                templateUrl: '/angular-multi-step-form/partials/example.html',
             })
             .otherwise('/home');
     }
 ])
 
-.controller('HowToInstallCtrl', [
+.controller('GettingStartedCtrl', [
     '$scope',
     function ($scope) {
+        $scope.exampleId = 1;
+
         $scope.steps = [
             {
-                template: '<div class="well">Install with bower, or download sources</div>',
+                templateUrl: '/angular-multi-step-form/examples/1/step1.html',
                 title: 'Get the source'
             },
             {
-                template: '<div class="well">Include source in your app and add dependency</div>',
+                templateUrl: '/angular-multi-step-form/examples/1/step2.html',
                 title: 'Add it to your app'
             },
             {
-                template: '<div class="well">Configure your multi step form</div>',
+                templateUrl: '/angular-multi-step-form/examples/1/step3.html',
                 title: 'Create your multi step forms / wizzards'
             },
             {
-                template: '<div class="well">More docs available on Github</div>',
-                title: 'Read the docs'
+                template: '<div class="well">Discover with the available examples and documentation the versatility of this lightweight angular module (6kb minified).</div>',
+                title: 'Discover the examples and read the docs!'
             }
         ];
     }
