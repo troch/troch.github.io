9a912b35f46788d6322cf03b5ee44cb92c0723c9
Thomas Roch <thomas.c.roch@gmail.com>
2015-06-16 18:56:27 +0100

    Improve look of examples

--- angular-multi-step-form/scripts/partials.js
+++ angular-multi-step-form/scripts/partials.js
@@ -3,12 +3,12 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
     '<div ng-include="\'examples/\' + exampleId + \'/partial.html\'"></div>\n' +
     '\n' +
     '<tabset>\n' +
-    '    <tab heading="CSS">\n' +
-    '        <div hljs include="\'examples/\' + exampleId + \'/styles.css\'" language="css"></div>\n' +
-    '    </tab>\n' +
-    '\n' +
     '    <tab heading="HTML">\n' +
     '        <div hljs include="\'examples/\' + exampleId + \'/partial.html\'" language="html"></div>\n' +
+    '    </tab>\n' +
+    '\n' +
+    '    <tab heading="CSS">\n' +
+    '        <div hljs include="\'examples/\' + exampleId + \'/styles.css\'" language="css"></div>\n' +
     '    </tab>\n' +
     '</tabset>\n' +
     '');
@@ -19,12 +19,12 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
     '<div ng-include="\'examples/\' + exampleId + \'/partial.html\'"></div>\n' +
     '\n' +
     '<tabset>\n' +
-    '    <tab heading="JavaScript">\n' +
-    '        <div hljs include="\'examples/\' + exampleId + \'/steps.js\'" language="javascript"></div>\n' +
-    '    </tab>\n' +
-    '\n' +
     '    <tab heading="HTML">\n' +
     '        <div hljs include="\'examples/\' + exampleId + \'/partial.html\'" language="html"></div>\n' +
+    '    </tab>\n' +
+    '\n' +
+    '    <tab heading="JavaScript">\n' +
+    '        <div hljs include="\'examples/\' + exampleId + \'/steps.js\'" language="javascript"></div>\n' +
     '    </tab>\n' +
     '</tabset>\n' +
     '');
@@ -32,14 +32,17 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
 
 angular.module('msfDemo').run(['$templateCache', function($templateCache) {
   $templateCache.put('/angular-multi-step-form/partials/home.html',
+    '<h3>Welcome</h3>\n' +
+    '\n' +
     '<p>\n' +
     '    <strong>multiStepForm</strong> is an angular module to create multi step forms and wizards. Create your steps like your would\n' +
     '    create your views with ngRoute or ui-router!\n' +
     '</p>\n' +
     '\n' +
-    '<p>It is lightweight (6kb minified) but extremely versatile and powerful. Discover what it is capable of with the following examples</p>\n' +
+    '<p>It is lightweight (6kb minified) but extremely versatile and powerful, and makes no assumption on how you want to design your multi-step\n' +
+    'form or wizard. Discover what it is capable of with the following examples and documentation.</p>\n' +
     '\n' +
-    '<br>\n' +
+    '\n' +
     '<h3>Examples</h3>\n' +
     '\n' +
     '<ul>\n' +
@@ -68,7 +71,27 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
     '    </li>\n' +
     '\n' +
     '    <li>\n' +
-    '        More to come\n' +
+    '        <a href="#/with-modal"><strong>In a modal:</strong> a multi-step form inside an angular-ui bootstrap modal</a>\n' +
+    '    </li>\n' +
+    '</ul>\n' +
+    '\n' +
+    '<h3>Docs (on GitHub)</h3>\n' +
+    '\n' +
+    '<ul>\n' +
+    '    <li>\n' +
+    '        <a href="https://github.com/troch/angular-multi-step-form/blob/master/docs/configuring-steps.md"><strong>Configuring your steps</strong></a>\n' +
+    '    </li>\n' +
+    '\n' +
+    '    <li>\n' +
+    '        <a href="https://github.com/troch/angular-multi-step-form/blob/master/docs/scopes.md"><strong>Steps and directive scopes</strong></a>\n' +
+    '    </li>\n' +
+    '\n' +
+    '    <li>\n' +
+    '        <a href="https://github.com/troch/angular-multi-step-form/blob/master/docs/multi-step-instance.md"><strong>The multi-step form instance object</strong></a>\n' +
+    '    </li>\n' +
+    '\n' +
+    '    <li>\n' +
+    '        <a href="https://github.com/troch/angular-multi-step-form/blob/master/docs/steps-lifecycle.md"><strong>Animations, navigation, callbacks</strong></a>\n' +
     '    </li>\n' +
     '</ul>\n' +
     '');
@@ -95,11 +118,11 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
 angular.module('msfDemo').run(['$templateCache', function($templateCache) {
   $templateCache.put('/angular-multi-step-form/examples/1/step1.html',
     '<div class="well">\n' +
-    '    <h4>Install with bower</h4>\n' +
+    '    <h3>Install with bower</h3>\n' +
     '\n' +
     '    <div hljs source="\'bower install --save angular-multi-step-form\'" language="sh"></div>\n' +
     '\n' +
-    '    <h4>Download from Github</h4>\n' +
+    '    <h3>Download from Github</h3>\n' +
     '\n' +
     '    <a href="https://github.com/troch/angular-multi-step-form/tree/master/dist">https://github.com/troch/angular-multi-step-form/tree/master/dist</a>\n' +
     '</div>\n' +
@@ -109,15 +132,15 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
 angular.module('msfDemo').run(['$templateCache', function($templateCache) {
   $templateCache.put('/angular-multi-step-form/examples/1/step2.html',
     '<div class="well">\n' +
-    '    <h4>Include it in your application</h4>\n' +
+    '    <h3>Include it in your application</h3>\n' +
     '\n' +
     '    <div hljs include="\'/angular-multi-step-form/examples/1/include-in-app\'" language="javascript"></div>\n' +
     '</div>\n' +
     '\n' +
-    '<script type="text/ng-template" id="examples/1/include-in-app">\n' +
-    '    angular.module(\'myApp\', [\n' +
-    '        \'multiStepForm\'\n' +
-    '    ]);\n' +
+    '<script type="text/ng-template" id="/angular-multi-step-form/examples/1/include-in-app">\n' +
+    'angular.module(\'myApp\', [\n' +
+    '    \'multiStepForm\'\n' +
+    ']);\n' +
     '</script>\n' +
     '');
 }]);
@@ -125,24 +148,24 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
 angular.module('msfDemo').run(['$templateCache', function($templateCache) {
   $templateCache.put('/angular-multi-step-form/examples/1/step3.html',
     '<div class="well">\n' +
-    '    <h4>Create your steps:</h4>\n' +
+    '    <h3>Create your steps:</h3>\n' +
     '\n' +
     '    <div hljs include="\'/angular-multi-step-form/examples/1/create-your-steps\'" language="javascript"></div>\n' +
     '\n' +
-    '    <h4>Run them in your view:</h4>\n' +
+    '    <h3>Run them in your view:</h3>\n' +
     '\n' +
     '    <div hljs include="\'/angular-multi-step-form/examples/1/run-your-steps\'" language="html"></div>\n' +
     '</div>\n' +
     '\n' +
-    '<script type="text/ng-template" id="examples/1/create-your-steps">\n' +
-    '    $scope.steps = [\n' +
-    '        {template: \'Hello <button class="btn btn-default" ng-click="$nextStep()">Next</button>\'},\n' +
-    '        {template: \'World <button class="btn btn-default" ng-click="$previousStep()">Previous</button>\'}\n' +
-    '    ];\n' +
+    '<script type="text/ng-template" id="/angular-multi-step-form/examples/1/create-your-steps">\n' +
+    '$scope.steps = [\n' +
+    '    {template: \'Hello <button class="btn btn-default" ng-click="$nextStep()">Next</button>\'},\n' +
+    '    {template: \'World <button class="btn btn-default" ng-click="$previousStep()">Previous</button>\'}\n' +
+    '];\n' +
     '</script>\n' +
     '\n' +
-    '<script type="text/ng-template" id="examples/1/run-your-steps">\n' +
-    '    <multi-step-container steps="steps"></multi-step-container>\n' +
+    '<script type="text/ng-template" id="/angular-multi-step-form/examples/1/run-your-steps">\n' +
+    '<multi-step-container steps="steps"></multi-step-container>\n' +
     '</script>\n' +
     '');
 }]);
@@ -176,6 +199,8 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
 angular.module('msfDemo').run(['$templateCache', function($templateCache) {
   $templateCache.put('/angular-multi-step-form/examples/2/step1.html',
     '<div class="well">\n' +
+    '    <h3>Using forms</h3>\n' +
+    '\n' +
     '    <p>You can use forms in steps, however this doesn\'t change much. Each step can take a property <strong>hasForm</strong> (true or false, false by default)\n' +
     '    but this property is harmless: it doesn\'t do anything at all.</p>\n' +
     '\n' +
@@ -189,6 +214,8 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
 angular.module('msfDemo').run(['$templateCache', function($templateCache) {
   $templateCache.put('/angular-multi-step-form/examples/2/step2.html',
     '<div class="well">\n' +
+    '    <h3>Share step validity</h3>\n' +
+    '\n' +
     '    <p>\n' +
     '        However if you want to allow step navigation in your header or footer, you will need to share form validation information from your step view to the rest of\n' +
     '        the component. There are a few ways to do this:\n' +
@@ -211,6 +238,8 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
 angular.module('msfDemo').run(['$templateCache', function($templateCache) {
   $templateCache.put('/angular-multi-step-form/examples/2/step3.html',
     '<div class="well">\n' +
+    '    <h3>Automatically share step validity</h3>\n' +
+    '\n' +
     '    <p>\n' +
     '        In practive, you are likely to want to watch your form validity status and update the associated form step object:\n' +
     '        the <strong>formStepValidity</strong> directive does just that for you. Simply put it on your form element (or inside your\n' +
@@ -243,7 +272,9 @@ angular.module('msfDemo').run(['$templateCache', function($templateCache) {
 angular.module('msfDemo').run(['$templateCache', function($templateCache) {
   $templateCache.put('/angular-multi-step-form/examples/2/step4.html',
     '<div class="well">\n' +
-    '    Congratulations! You have passed!\n' +
+    '    <h3>Congratulations</h3>\n' +
+    '\n' +
+    '    You have passed!\n' +
     '</div>\n' +
     '');
 }]);
